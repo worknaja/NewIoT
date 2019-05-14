228c8f454006fe4608a9282c50bb0180928facd3
worknaja <nockandriod@gmail.com>
2019-05-14 15:51:15 +0700

    final

--- app/src/main/java/com/example/nsx/newiot/MyAlert.java
+++ app/src/main/java/com/example/nsx/newiot/MyAlert.java
@@ -1,8 +1,8 @@
 package com.example.nsx.newiot;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+        import android.content.Context;
+        import android.content.DialogInterface;
+        import android.support.v7.app.AlertDialog;
 
 public class MyAlert {
 
